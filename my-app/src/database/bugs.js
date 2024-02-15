ddce02877496caaf104f8658d5e762396f7c0478
jonique98 <tnals9899@naver.com>
2024-02-15 22:14:31 +0900

    fix bugs rank

--- my-app/src/database/bugs.js
+++ my-app/src/database/bugs.js
@@ -2,18 +2,27 @@ const axios = require('axios');
 
 const bugsCrawler = () => {
 
-const postData = {
-  key1: 'value1',
-  key2: 'value2',
-};
+// const postData = {
+//   key1: 'value1',
+//   key2: 'value2',
+// };
 
-axios.post('https://e00tl528jj.execute-api.ap-northeast-2.amazonaws.com/sumjo-stage/sumjo', postData)
+// axios.post(' https://m2g27gtzoj.execute-api.ap-northeast-2.amazonaws.com/default/sumjo', postData)
+//   .then(response => {
+//     console.log(response.data);
+//   })
+//   .catch(error => {
+//     console.error('Error sending POST request:', error);
+//   });
+// };
+
+axios.get('https://rhtfz29a97.execute-api.ap-northeast-2.amazonaws.com/default/sumjo')
   .then(response => {
     console.log(response.data);
   })
   .catch(error => {
-    console.error('Error sending POST request:', error);
+    console.error('Error sending GET request:', error);
   });
-};
+}
 
 module.exports = bugsCrawler;

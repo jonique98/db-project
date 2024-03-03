cb882953562e492374e378519801ed659653d770
jonique98 <tnals9899@naver.com>
2024-03-03 15:09:04 +0900

    build

---
+++ my-app/build/precache-manifest.a775a478bac75d60c6d07b3a342094da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac2570c1c91071a6c5b35fc5ce1d62e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61600366fc418b5c28b0",
+    "url": "/static/css/2.cc82f8aa.chunk.css"
+  },
+  {
+    "revision": "0e98e005d94424acf7b2",
+    "url": "/static/css/main.d8fb79bf.chunk.css"
+  },
+  {
+    "revision": "61600366fc418b5c28b0",
+    "url": "/static/js/2.f5b0fd9b.chunk.js"
+  },
+  {
+    "revision": "0e98e005d94424acf7b2",
+    "url": "/static/js/main.e7846702.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
